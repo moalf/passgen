74ca17f4abb080d5eebfb6d2b0a9b7044f97871b
AJ Molero <ajmolero@gmail.com>
2024-02-02 16:27:43 -0500

    fix serving in container

--- main.go
+++ main.go
@@ -3,19 +3,20 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/moalf/passgen/server"
 )
 
 const (
-	address = "127.0.0.1"
+	address = "0.0.0.0"
 	port    = 8080
 )
 
 func main() {
 	server := server.NewHttpServer(address, port)
 
-	fmt.Printf("Starting server on %s:%d\n", address, port)
+	fmt.Printf("%s passgen started...\n", time.Now().Format("2006-01-02 15:04:05"))
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
